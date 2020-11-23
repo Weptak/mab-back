c52957a364cdd328d5d61129a63e2f2889d176d6
Yorick Weenen <71253572+Weptak@users.noreply.github.com>
2020-11-23 10:49:40 +0100

    Set up using Boot

---
+++ src/main/java/be/bruxellesformation/mabback/MabBackendApplication.java
@@ -0,0 +1,13 @@
+package be.bruxellesformation.mabback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MabBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/bruxellesformation/mabback/MabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.bruxellesformation.mabback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
